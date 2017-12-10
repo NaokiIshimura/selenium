7d774a30e65a91cbb35bca34b523adb27c9e5c59
Naoki Ishimura <mirontest1460@gmail.com>
2017-12-10 16:25:39 +0900

    Refactoring

--- sample/selenium.rb
+++ sample/selenium.rb
@@ -1,42 +1,70 @@
 #! /usr/bin/ruby
 
-require 'selenium-webdriver'
+# ブラウザを起動する
+# @params [String] 起動するブラウザ(chrome/firefox)
+# @return [Driver] WebDriver
+def browser_start(browser)
 
-# chromeを起動する
-Selenium::WebDriver::Chrome.driver_path = './webdriver/chromedriver'
-# driver = Selenium::WebDriver.for :chrome
+  require 'selenium-webdriver'
 
-# prefs = {
-#   download: {
-#     prompt_for_download: false,
-#     default_directory: '~/Downloads'
-#   }
-# }
-# driver = Selenium::WebDriver.for :chrome, prefs: prefs
+  case browser
+    when 'chrome'
 
-prefs = {
-    prompt_for_download: false,
-    default_directory: '~/Downloads'
-}
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_preference(:download, prefs)
-driver = Selenium::WebDriver.for :chrome, options: options
+      # chromeを起動する
+      Selenium::WebDriver::Chrome.driver_path = './webdriver/chromedriver'
+      # driver = Selenium::WebDriver.for :chrome
 
-# firefoxを起動する（app, プロファイル指定なし）
-# driver = Selenium::WebDriver.for :firefox
+      # prefs = {
+      #   download: {
+      #     prompt_for_download: false,
+      #     default_directory: '~/Downloads'
+      #   }
+      # }
+      # driver = Selenium::WebDriver.for :chrome, prefs: prefs
 
-# firefoxを起動する（appのディレクトリを指定、プロファイルを指定）
-# Selenium::WebDriver::Firefox.path="/Applications/Firefox.app/Contents/MacOS/firefox-bin"
-# driver = Selenium::WebDriver.for(:firefox, :profile => "default-1413197258474")
+      prefs = {
+          prompt_for_download: false,
+          default_directory: '~/Downloads'
+      }
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_preference(:download, prefs)
+      driver = Selenium::WebDriver.for :chrome, options: options
 
-# オプション
-# 暗黙的なwaitを設定
-driver.manage.timeouts.implicit_wait = 10
+    when 'firefox'
+
+      # firefoxを起動する（app, プロファイル指定なし）
+      driver = Selenium::WebDriver.for :firefox
+
+      # firefoxを起動する（appのディレクトリを指定、プロファイルを指定）
+      Selenium::WebDriver::Firefox.path="/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+      driver = Selenium::WebDriver.for(:firefox, :profile => "default-1413197258474")
+
+  end
+
+  ##
+  # オプション
+  # 暗黙的なwaitを設定
+  driver.manage.timeouts.implicit_wait = 10
+
+  p driver.class
+
+  return driver
+end
+
+
+##
+# main
+
+# 引数が指定されてない場合にパラメータを設定する
+keyword = ARGV[0] ||= 'selenium'
+browser = ARGV[1] ||= 'chrome'
+
+# ブラウザを起動する
+driver = browser_start(browser)
 
 # サイトにアクセスする
 base_url = 'https://www.google.co.jp/search?q='
-word = ARGV[0]
-url = base_url + word
+url = base_url + keyword
 
 driver.get url
 
